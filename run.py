c0f217cb02d34693e176d6ded0743311446ae1ac
Ubuntu <ubuntu@ip-172-31-8-248.us-west-1.compute.internal>
2023-05-13 08:14:17 -0700

    debug

--- run.py
+++ run.py
@@ -151,7 +151,7 @@ def mciv_test(config_file):
         fp.write(f"{node_uct_explore} ; ")
         fp.write(f"{num_node_expand} ; ")
         fp.write(f"{n_opt_local} ")
-        fp.write(f"{time_jit:.4f} \n"
+        fp.write(f"{time_jit:.4f} \n")
 
         fcntl.flock(fp, fcntl.LOCK_UN)
 
