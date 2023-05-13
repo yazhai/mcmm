b0f6b7e47592baf773be04f389c43329e319813c
yazhai <22529912+yazhai@users.noreply.github.com>
2023-05-12 22:23:53 -0700

    feat: add mciv run.py

---
+++ run.py
@@ -0,0 +1,139 @@
+import sys, os, string, random
+import time, json, fcntl
+
+
+from src.test_functions import *
+from src.mciv import MCIV
+
+
+def mciv_test(config_file):
+    # ===========================================================================
+    # read the json file
+    config = json.loads(open(config_file).read())
+
+    # interpret the config file
+    fn_class = eval(config["objective"]["function"])
+    dims = config.get("objective", {}).get("dims", 2)
+    lb = config.get("objective", {}).get("lb", None)
+    ub = config.get("objective", {}).get("ub", None)
+    init_args = config.get("initial", {})
+    optimize_args = config.get("optimize", {})
+
+    # ===========================================================================
+    # folders for the results
+    temp_foler = "temp"
+    save_dir = "./benchmark_mciv/"
+    os.makedirs(temp_foler, exist_ok=True)
+    os.makedirs(save_dir, exist_ok=True)
+
+    # ===========================================================================
+    # define temperary file saving the print output
+    temp_filename = "".join(
+        random.choices(string.ascii_lowercase + string.digits, k=40)
+    )
+    temp_file = os.path.join(temp_foler, temp_filename)
+
+    # Backup original stdout and stderr
+    orig_stdout = sys.stdout
+    orig_stderr = sys.stderr
+
+    # Open file for writing
+    temp_std = open(temp_file, "w")
+
+    # Redirect stdout and stderr to the file
+    sys.stdout = temp_std
+    sys.stderr = temp_std
+
+    # ===========================================================================
+    # run the optimization
+    fn = fn_class(dims=dims)
+    if (lb is None) or (ub is None):
+        lb, ub = fn.get_default_bounds()
+
+    alg = MCIV(fn, lb, ub, **init_args)
+    start = time.time()
+    try:
+        print(f"Start optimization {fn_class.__name__} at dim {dims}")
+        best_y = alg.optimize(**optimize_args)
+    except:
+        best_y = alg.root.y
+    end = time.time()
+    eclipsed = end - start
+
+    # Reset stdout and stderr back to their original values before your program finishes
+    temp_std.close()
+    sys.stdout = orig_stdout
+    sys.stderr = orig_stderr
+
+    # ===========================================================================
+    # save the results
+
+    # get configurations from the object
+    max_iter = alg.max_iterations
+    n_opt_local = alg.n_opt_local
+    node_uct_lb_coeff = alg.node_uct_lb_coeff
+    node_uct_box_coeff = alg.node_uct_box_coeff
+    node_uct_explore = alg.node_uct_explore
+    num_node_expand = alg.num_node_expand
+    seed = alg.seed
+
+    # get history from the object
+    history = alg.history
+    total_sample = history.shape[0]
+    try:
+        first_reach_sample = np.where(history[:, 0] == best_y)[0][0]
+    except:
+        first_reach_sample = total_sample
+    first_reach_time = first_reach_sample / total_sample * eclipsed
+
+    # std output rename
+    save_filename = f"{fn_class.__name__}_{dims}d_{temp_filename}"
+    save_file = os.path.join(save_dir, save_filename)
+    try:
+        os.popen("mv {} {}".format(temp_file, save_file))
+    except:
+        pass
+
+    # dump history file
+    history_filename = os.path.join(save_dir, save_filename + ".npy")
+    np.save(history_filename, history)
+
+    # save the results
+    result_filename = f"{fn_class.__name__}.txt"
+    result_file = os.path.join(save_dir, result_filename)
+
+    with open(result_file, "a+") as fp:
+        fcntl.flock(fp, fcntl.LOCK_EX)
+
+        # write title line
+        fp.seek(0)
+        c = fp.readlines()
+        if len(c) == 0:
+            fp.write(
+                "#fn_name ; dims ; best_y ; seed ; total_time ; first_reach_time ; total_sample ; first_reach_sample; max_iteration ; node_uct_lb_coeff ; node_uct_box_coeff ; node_uct_explore ; num_node_expand ; n_opt_local \n"
+            )
+
+        # write results
+        fp.write(f"{fn_class.__name__} ; ")
+        fp.write(f"{dims} ; ")
+        fp.write(f"{best_y:.6f} ; ")
+        fp.write(f"{seed} ; ")
+        fp.write(f"{eclipsed:.4f} ; ")
+        fp.write(f"{first_reach_time:.4f} ; ")
+        fp.write(f"{total_sample} ; ")
+        fp.write(f"{first_reach_sample} ; ")
+        fp.write(f"{max_iter} ; ")
+        fp.write(f"{node_uct_lb_coeff} ; ")
+        fp.write(f"{node_uct_box_coeff} ; ")
+        fp.write(f"{node_uct_explore} ; ")
+        fp.write(f"{num_node_expand} ; ")
+        fp.write(f"{n_opt_local} \n")
+
+        fcntl.flock(fp, fcntl.LOCK_UN)
+
+    return
+
+
+if __name__ == "__main__":
+    json_file = sys.argv[1]
+    mciv_test(json_file)
